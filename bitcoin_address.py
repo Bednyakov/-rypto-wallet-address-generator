fde86543d789071e9bc1a9864927483935d793e1
Artem Bednyakov <artb@list.ru>
2024-12-04 12:08:31 +0200

    added multiprocessing

--- bitcoin_address.py
+++ bitcoin_address.py
@@ -2,6 +2,8 @@ import os
 import requests
 from bitcoinlib.keys import HDKey
 from bitcoinlib.transactions import Transaction, Input, Output
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import multiprocessing
 from bitcoinlib.wallets import Wallet
 from bitcoinlib.services.services import Service
 
@@ -13,7 +15,7 @@ def generate_bitcoin_address_segwit(start_pattern="", end_pattern=""):
     которая использует меньше данных для транзакций 
     и имеет более низкие комиссии по сравнению 
     с более старыми форматами.
-    Адреса SegWit начинаются с "bc1"
+    Адреса SegWit начинаются с "bc1q"
     """
     while True:
         # Генерируем случайный приватный ключ
@@ -30,6 +32,26 @@ def generate_bitcoin_address_segwit(start_pattern="", end_pattern=""):
             print(f"Найден адрес: {address}")
             print(f"Приватный ключ (HEX): {private_key_bytes.hex()}")
             return address, private_key_bytes.hex()
+        
+
+def parallel_address_search(start_pattern="", end_pattern=""):
+    """
+    Запуск генерации адресов параллельно на всех доступных ядрах процессора.
+    """
+    num_cores = multiprocessing.cpu_count()  # Количество доступных ядер
+    print(f"Используется {num_cores} ядер для параллельного поиска...")
+
+    with ProcessPoolExecutor(max_workers=num_cores) as executor:
+        # Запускаем задачу на каждом ядре
+        futures = [executor.submit(generate_bitcoin_address_segwit, start_pattern, end_pattern) for _ in range(num_cores)]
+
+        # Ждем завершения любой из задач
+        for future in as_completed(futures):
+            if future.result():
+                # Если одна из задач завершилась успешно, отменяем остальные
+                executor.shutdown(wait=False, cancel_futures=True)
+                print("Адрес найден!")
+                return future.result()
 
 
 def get_balance_bitcoin(address):
@@ -87,5 +109,10 @@ def send_bitcoin_directly(private_key, recipient_address, amount_btc):
     print(f"Транзакция отправлена! Хэш: {tx_id}")
 
 if __name__ == "__main__":
-      address, private_key = generate_bitcoin_address_segwit(start_pattern="", end_pattern="artem")
-      get_balance_bitcoin(address)
+    result = parallel_address_search(start_pattern="", end_pattern="artem")
+    if result:
+        address, private_key = result
+    print(f"Найденный адрес: {address}")
+    print(f"Приватный ключ: {private_key}")
+#   address, private_key = generate_bitcoin_address_segwit(start_pattern="", end_pattern="artem")
+    get_balance_bitcoin(address)
